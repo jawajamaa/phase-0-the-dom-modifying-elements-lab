8f80ec3f4d86a353d2a023d5afb62dbbebef69fc
Timothy Ryon <tim@timryon.com>
2023-04-20 13:33:50 -0400

    finished DOM assignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Write your code here!
+const main = document.getElementById("main");
+main.remove();
+
+const newHeader = document.createElement("h1");
+
+newHeader.id = "victory";
+
+newHeader.textContent = ("TIM is the champion");
+
+
+/*
+document.body.append(newHeader);
+
+const element = document.createElement("div");
+      document.body.append(element);
+      const ul = document.createElement("ul");
+      for (let i = 0; i < 3; i++) {
+        const li = document.createElement("li");
+        li.textContent = (i + 1).toString();
+        ul.append(li);
+      }
+      element.append(ul);
+*/
+      
